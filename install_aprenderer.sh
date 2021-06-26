81b1d48585adbce648aab4d785facfaefe11b09f
sam0402 <sam0402@gmail.com>
2021-06-26 09:56:32 +0800

    Update install_aprenderer.sh

--- install_aprenderer.sh
+++ install_aprenderer.sh
@@ -5,9 +5,9 @@
 
 useBusybox
 TARGET=`cat /etc/sysconfig/backup_device`
-ceo
-wget https://raw.githubusercontent.com/sam0402/pcp-44.1KHz/master/pcp-aplayer_render.tcz
-ce
+cd /mnt/{TARGATE}
 echo pcp-aplayer_render.tcz >>onboot.lst
+cd optional
+wget https://raw.githubusercontent.com/sam0402/pcp-44.1KHz/master/pcp-aplayer_render.tcz
 
 pcp_write_var_to_config USER_COMMAND_3 "aplayer; aprenderer"
