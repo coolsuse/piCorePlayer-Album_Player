6bef6c5e1f604ffb20cef0c7a1bd59d1c2bb48ef
sam0402 <sam0402@gmail.com>
2021-06-27 15:50:27 +0800

    Update install_aprenderer.sh

--- install_aprenderer.sh
+++ install_aprenderer.sh
@@ -8,6 +8,6 @@ TARGET=`cat /etc/sysconfig/backup_device`
 cd /mnt/$TARGET
 echo pcp-aplayer_render.tcz >>onboot.lst
 cd optional
-wget https://raw.githubusercontent.com/sam0402/pcp-44.1KHz/master/pcp-aplayer_render.tcz
+wget https://raw.githubusercontent.com/sam0402/pcp-Album_Player/master/pcp-aplayer_render.tcz
 
 pcp_write_var_to_config USER_COMMAND_3 "aplayer;aprenderer"
