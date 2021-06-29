aa4b060285cf1a8065a03634afc617b0c29b0710
sam0402 <sam0402@gmail.com>
2021-06-29 09:59:34 +0800

    Update install_aprenderer.sh

--- install_aprenderer.sh
+++ install_aprenderer.sh
@@ -10,7 +10,7 @@ echo pcp-aplayer_render.tcz >>onboot.lst
 cd optional
 wget https://raw.githubusercontent.com/sam0402/piCorePlayer-Album_Player/master/pcp-aplayer_render.tcz
 
-pcp_write_var_to_config USER_COMMAND_3 "sudo%20taskset%20-c%203%20aplayer;%20sudo%20taskset%20-c%203%20aprenderer"
+pcp_write_var_to_config USER_COMMAND_3 "aplayer;aprenderer"
 echo "Rebooting..."
 sleep 3
 pcp br
